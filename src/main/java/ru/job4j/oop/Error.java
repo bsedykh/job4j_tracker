a9b7e0324441dae02bc9c6679400c06eb37b8da2
Boris Sedykh <sedykh.boris@yandex.ru>
2023-10-18 15:34:00 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,31 @@
+package ru.job4j.oop;
+
+public class Error {
+
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error() {
+
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void print() {
+        System.out.println("Active: " + active);
+        System.out.println("Status: " + status);
+        System.out.println("Message: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error defaultError = new Error();
+        defaultError.print();
+        Error customError = new Error(true, 10, "test");
+        customError.print();
+    }
+}
