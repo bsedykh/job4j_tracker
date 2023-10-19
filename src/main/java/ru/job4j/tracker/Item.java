7ee0ab7be3e9079525490b7f86891a80d1be7c07
Boris Sedykh <sedykh.boris@yandex.ru>
2023-10-20 00:28:03 +0300

    8. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,9 +1,11 @@
 package ru.job4j.tracker;
 
-public class Item {
+import java.time.LocalDateTime;
 
+public class Item {
     private int id;
     private String name;
+    private LocalDateTime created = LocalDateTime.now();
 
     public Item() {
     }
@@ -31,5 +33,9 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
     }
 }
