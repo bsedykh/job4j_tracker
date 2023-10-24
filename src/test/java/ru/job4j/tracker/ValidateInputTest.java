c7c16f387adbaae0b4a2d43ca3a7df4340df140f
Boris Sedykh <sedykh.boris@yandex.ru>
2023-10-24 22:34:28 +0300

    2.1. Тест на ValidateInput [#311887]

---
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -0,0 +1,52 @@
+package ru.job4j.tracker;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ValidateInputTest {
+    @Test
+    public void whenInvalidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"one", "1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(1);
+    }
+
+    @Test
+    public void whenValidPositiveInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(1);
+    }
+
+    @Test
+    public void whenValidNegativeInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"-1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(-1);
+    }
+
+    @Test
+    public void whenValidMultipleInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"-50", "200", "3"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int[] expected = {-50, 200, 3};
+        for (int number : expected) {
+            assertThat(input.askInt("Enter menu:")).isEqualTo(number);
+        }
+    }
+}
