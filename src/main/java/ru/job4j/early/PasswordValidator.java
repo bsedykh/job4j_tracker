41d6b42be03ea8fdb0c2bc60a21788f953576cc1
Boris Sedykh <sedykh.boris@yandex.ru>
2023-10-25 13:09:44 +0300

    4. Принципы раннего возврата и охранных выражений [#504875 #455280]

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -50,6 +50,9 @@ public class PasswordValidator {
             if (!Character.isLetterOrDigit(symbol)) {
                 hasSpecial = true;
             }
+            if (hasUpCase && hasLowCase && hasDigit && hasSpecial) {
+                break;
+            }
         }
         if (!hasUpCase) {
             throw new IllegalArgumentException(
