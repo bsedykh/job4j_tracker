d28d0a9400c5e7668addef4ce5dcf1c69bfff432
Boris Sedykh <sedykh.boris@yandex.ru>
2023-11-01 11:15:17 +0300

    4. Телефонный справочник на базе ArrayList [#41598 #456819]

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -12,17 +12,13 @@ public class PhoneDictionary {
     public ArrayList<Person> find(String key) {
         ArrayList<Person> result = new ArrayList<>();
         for (Person person : persons) {
-            if (personContainsKey(person, key)) {
+            if (person.getName().contains(key)
+                    || person.getSurname().contains(key)
+                    || person.getPhone().contains(key)
+                    || person.getAddress().contains(key)) {
                 result.add(person);
             }
         }
         return result;
-    }
-
-    private static boolean personContainsKey(Person person, String key) {
-        return person.getName().contains(key)
-                || person.getSurname().contains(key)
-                || person.getPhone().contains(key)
-                || person.getAddress().contains(key);
     }
 }
