0121c84f3fb4c426867193fac2425b34ec432276
Boris Sedykh <sedykh.boris@yandex.ru>
2023-11-02 17:42:13 +0300

    0. Map, HashMap [#212652 #457087]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,6 +6,9 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("abc@example.com", "Ivanov Ivan Ivanovich");
+        map.put("abc@example.com", "Petrov Petr Petrovich");
+        map.put("test@example.com", "Sidorov Sidor Sidorovich");
+        map.put("work@example.com", "Sergeev Sergey Sergeevich");
         for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println(key + " = " + value);
