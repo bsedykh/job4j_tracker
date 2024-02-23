b17e39f5fb1262067907cbdd8c9b3f4ed6e49cfe
Boris Sedykh <sedykh.boris@yandex.ru>
2024-02-23 18:19:17 +0300

    1. Конфигурация Hibernate [#6872]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -3,12 +3,19 @@ package ru.job4j.tracker;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "items")
 @Data
 public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     private String name;
+
     @EqualsAndHashCode.Exclude
     private LocalDateTime created = LocalDateTime.now();
 
